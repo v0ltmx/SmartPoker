b222bc6980635604b67c4620b8ce787fa0097ef8
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2021-03-06 17:52:54 -0300

    up script

--- script.m
+++ script.m
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 pkg load image
 
 imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', 'K_paus.jpg');
@@ -13,22 +13,4 @@ B = img_gray;
 B(B<=LIMIAR) = 0;
 B(B>LIMIAR) = 255;
 
-
-=======
-pkg load image
-
-imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', 'K_paus.jpg');
-
-img = imread(imagem);
-
-img_gray = rgb2gray(img);
-
-LIMIAR = 220;
-
-B = img_gray;
-B(B<=LIMIAR) = 0;
-B(B>LIMIAR) = 255;
-
-
->>>>>>> bbfa009986fdb46e9b87a5a0113128ea96269b13
 figure, imshow(B);
