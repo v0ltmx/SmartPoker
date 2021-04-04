d9b936c3911241e8fc702f9e8996e4ca1e408dbc
v0ltmx <lucasgabrielg97@ufrn.edu.br>
2021-04-03 21:26:00 -0300

    add segmentacao

--- script.m
+++ script.m
@@ -1,7 +1,11 @@
-<<<<<<< HEAD
+clear all
 pkg load image
 
-imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', 'K_paus.jpg');
+imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', '8_ouro.jpg');
+
+figure('Name','Imagem Lida');
+imshow(imagem);
+
 
 img = imread(imagem);
 
@@ -13,22 +17,83 @@ B = img_gray;
 B(B<=LIMIAR) = 0;
 B(B>LIMIAR) = 255;
 
+figure('Name', 'Imagem Binarizada'); 
+imshow(B);
 
-=======
-pkg load image
+#peq = B;
 
-imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', 'K_paus.jpg');
+#Cortando a imagem 
+##I2 = B(500:1400, 1100:1630,:);
+I2=B;
+##
+#peq = I2;
 
-img = imread(imagem);
+#Redução de coordenadas
+x=1;
+for i=2:2:size(I2,1)
+    y=1;
+    for j=2:2:size(I2,2)
+        peq(x,y) = I2(i,j);
+        y=y+1;
+    end
+    x=x+1;
+end
+    
+bool1 = 0;
+bool2 = 0;
+aux = 2;
+aux2 = 2;
+auy = 2;
+auy2 = 2;
 
-img_gray = rgb2gray(img);
+for i=10:size(peq,1)-1
+  
+      if(bool1 == 1 && bool2 == 1)
+        break;
+      endif
+  for j=10:size(peq,2)-1
+        if(peq(i,j,:)==255   
+          &&peq(i-1,j,:)==255
+          &&peq(i+1,j,:)==255
+          &&peq(i,j-1,:)==0
+          &&peq(i,j+1,:)==255
+          &&bool1==0)
+                     
+          
+           aux = i;
+           aux2 = j+7;      
+           bool1 = 1;
+            
+          
+        endif
+        
+          if(peq(i,j,:)==0 
+          &&peq(i-3,j,:)==255
+          &&peq(i+3,j,:)==255
+          &&peq(i,j+1,:)==0
+          &&peq(i,j-1,:)==0
+          &&bool2== 0)
+              
+            auy = i+150;
+            auy2 = j-10;
+            bool2 = 1;
+          
+        endif
+  endfor
+endfor
 
-LIMIAR = 220;
+aux
+aux2
+auy
+auy2
 
-B = img_gray;
-B(B<=LIMIAR) = 0;
-B(B>LIMIAR) = 255;
+I3 = peq(aux:auy, aux2:auy2,:); 
 
 
->>>>>>> bbfa009986fdb46e9b87a5a0113128ea96269b13
-figure, imshow(B);
+figure('Name', 'Imagem crop peq');
+imshow(peq);
+
+figure('Name', 'Imagem final');
+imshow(I3);
+
+I3
