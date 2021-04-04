a5948b0243a30ac7e2c8b471af9e54001cc63dde
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2021-04-03 21:39:03 -0300

    Update scriptSeguimentacao.m

--- scriptSeguimentacao.m
+++ scriptSeguimentacao.m
@@ -1,6 +1,8 @@
+
+clear all
 pkg load image
 
-imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', 'A_paus.jpg');
+imagem = strcat('C:\Users\lucas\Desktop\SmartPoker\banco\', '8_ouro.jpg');
 
 figure('Name','Imagem Lida');
 imshow(imagem);
@@ -19,41 +21,48 @@ B(B>LIMIAR) = 255;
 figure('Name', 'Imagem Binarizada'); 
 imshow(B);
 
+#peq = B;
+
+
+pkg load image
 
 
 #Cortando a imagem 
-I2 = B(500:1400, 1100:1630,:);
+##I2 = B(500:1400, 1100:1630,:);
+I2=B;
+##
+#peq = I2;
 
 
 #Redução de coordenadas
 x=1;
-for i=1:2:size(I2,1)
+for i=2:2:size(I2,1)
     y=1;
-    for j=1:2:size(I2,2)
+    for j=2:2:size(I2,2)
         peq(x,y) = I2(i,j);
         y=y+1;
     end
     x=x+1;
 end
-
-
     
 bool1 = 0;
 bool2 = 0;
+aux = 2;
+aux2 = 2;
+auy = 2;
+auy2 = 2;
 
-aux = 1;
-aux2 = 1;
-auy = 1;
-auy2 = 1;
+%%PrÃ©-processamento  && SeguimentaÃ§Ã£o%%
 
-##aux =[];
-##auy =[];
-for i=2:size(peq,1)-1
+img = imread(imagem);
+
+
+for i=10:size(peq,1)-1
   
       if(bool1 == 1 && bool2 == 1)
         break;
       endif
-  for j=2:size(peq,2)-1
+  for j=10:size(peq,2)-1
         if(peq(i,j,:)==255   
           &&peq(i-1,j,:)==255
           &&peq(i+1,j,:)==255
@@ -63,20 +72,20 @@ for i=2:size(peq,1)-1
                      
           
            aux = i;
-           aux2 = j;      
+           aux2 = j+7;      
            bool1 = 1;
             
           
         endif
         
-          if(peq(i,j,:)==255 
-          &&peq(i-1,j,:)==0
-          &&peq(i+1,j,:)==0
-          &&peq(i,j+1,:)==255
-          &&peq(i,j-1,:)==255
+          if(peq(i,j,:)==0 
+          &&peq(i-3,j,:)==255
+          &&peq(i+3,j,:)==255
+          &&peq(i,j+1,:)==0
+          &&peq(i,j-1,:)==0
           &&bool2== 0)
               
-            auy = i ;
+            auy = i+150;
             auy2 = j-10;
             bool2 = 1;
           
@@ -84,11 +93,21 @@ for i=2:size(peq,1)-1
   endfor
 endfor
 
+aux
+aux2
+auy
+auy2
+
 I3 = peq(aux:auy, aux2:auy2,:); 
+
 
 figure('Name', 'Imagem crop peq');
 imshow(peq);
 
-figure('Name', 'Imagem cortada');
+
+figure('Name', 'Imagem final');
 imshow(I3);
 
+I3
+
+figure, imshow(B);
